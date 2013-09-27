b6846753c1b7574fdc4f46fa3151b745ba6fbf23
rjsteinert <rj@rjsteinert.com>
2013-09-27 10:52:17 -0400

    Installation script. npm install; node install.js;

---
+++ install.js
@@ -0,0 +1,22 @@
+var _ = require('underscore')
+var sys = require('sys')
+var exec = require('child_process').exec;
+var fs = require('fs')
+function puts(error, stdout, stderr) { sys.puts(stdout) }
+  
+// Vars
+var couchUrl = 'http://127.0.0.1:5984' 
+var database = 'apps'
+var ignore = ['.git', 'package.json', 'node_modules', 'install.js']
+
+// Go
+fs.readdir('./', function(err, files) {
+  _.each(files, function(file){
+    if(ignore.indexOf(file) === -1) {
+      var cmd = 'couchapp push ' + file + '/app.js ' + couchUrl + '/' + database
+      console.log(cmd)
+      exec(cmd, puts)
+    }
+  })
+})
+
